36b2ec0b12999a40bbef9f557a35c88334c10425
shubham <heyshubham69@gmail.com>
2024-06-17 14:25:22 +0530

    UI related changes done

--- app/src/main/java/com/example/imagegridviewdemo/presentation/ImageGridScreen.kt
+++ app/src/main/java/com/example/imagegridviewdemo/presentation/ImageGridScreen.kt
@@ -1,5 +1,8 @@
 package com.example.imagegridviewdemo.presentation
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -8,19 +11,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,31 +48,64 @@ import com.example.imagegridviewdemo.data.Thumbnail
 @Composable
 fun ImageGridScreen(viewModel: ImageViewModel) {
     val imageDataList by viewModel.imageData.observeAsState()
+    var isInternetAvailable by remember { mutableStateOf(false) }
+
+    val connectivityManager =
+        LocalContext.current.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    DisposableEffect(connectivityManager) {
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                isInternetAvailable = true
+            }
+
+            override fun onLost(network: Network) {
+                isInternetAvailable = false
+            }
+        }
+
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
+
+        onDispose {
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        }
+    }
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
     ) {
-        Text(text = "Image Grid",
+        Text(
+            text = "Image Grid",
             style = MaterialTheme.typography.headlineSmall,
             modifier = Modifier
                 .fillMaxWidth()
                 .background(Color.LightGray)
-                .align(Alignment.CenterHorizontally)
         )
-        LazyVerticalGrid(
-            columns = GridCells
-                .Fixed(3),
-            verticalArrangement = Arrangement.spacedBy(6.dp),
-            horizontalArrangement = Arrangement.spacedBy(6.dp),
-            contentPadding = PaddingValues(4.dp),
-            //        modifier = Modifier.background(Color.LightGray)
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            imageDataList?.let {
-                items(it) { rowItems ->
-                    ThumbnailImage(rowItems.thumbnail)
+            Text(text = "Internet Status: ${if (isInternetAvailable) "Available" else "Not Available"}")
+        }
+        if (isInternetAvailable || imageDataList != null) {
+            LazyVerticalGrid(
+                columns = GridCells
+                    .Fixed(3),
+                verticalArrangement = Arrangement.spacedBy(6.dp),
+                horizontalArrangement = Arrangement.spacedBy(6.dp),
+                contentPadding = PaddingValues(4.dp),
+                //        modifier = Modifier.background(Color.LightGray)
+            ) {
+                imageDataList?.let {
+                    items(it) { rowItems ->
+                        ThumbnailImage(rowItems.thumbnail)
+                    }
                 }
             }
+        }else{
+            Text(text = "Data Not Available")
+
         }
-    }
+       }
 }
 
 @Composable
--- app/src/main/java/com/example/imagegridviewdemo/presentation/MainActivity.kt
+++ app/src/main/java/com/example/imagegridviewdemo/presentation/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.example.imagegridviewdemo.presentation
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.imagegridviewdemo.data.repository.ImageRepositoryImpl
 import com.example.imagegridviewdemo.domain.repository.ImageRepository
@@ -35,3 +53,4 @@ class MainActivity : ComponentActivity() {
 
     }
 }
+
